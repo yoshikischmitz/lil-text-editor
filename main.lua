ae17be3e53741e1ec3cd89a19381dcb7809a73f7
Yoshiki Schmitz <yoshikischmitz@outlook.com>
2020-04-22 20:52:20 +0900

    change position of cursor when mouse is clicked

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 
 local text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" 
-local cursor = 1
+local cursor = 10
+local hoverIndex = 0
 local lines = {} -- text split up into lines 
 local lineHeight = 1.5
 local fontSize = 18
@@ -58,7 +59,6 @@ function love.load()
 end
 
 function love.textinput(t)
-  print(table.concat(text))
   table.insert(text, cursor + 1, t)
   cursor = cursor + 1
 end
@@ -78,15 +78,29 @@ function love.keypressed(key)
   end
 end
 
-function love.update()
+function love.mousepressed()
   x = love.mouse:getX()
   y = love.mouse:getY()
   -- TODO make this nicer to account for subtle area in line gap
   lineIndex = 1 + math.floor(y / (fontSize * lineHeight))
-  if(lineIndex < table.getn(lines)) then
+  local chars = 1
+
+  if(lineIndex < table.getn(lines) + 1) then
     line = lines[lineIndex]
-    print(line)
-    print(line[1].char)
+    offset = 0
+    for i, charData in ipairs(line) do
+      if x < offset + charData.width then
+        cursor = chars
+        if lineIndex > 1 then
+          for i = 1, lineIndex - 1 do
+            cursor = cursor + table.getn(lines[i])
+          end
+        end
+        break
+      end
+      offset = offset + charData.width
+      chars = chars + 1
+    end
   end
 end
 
@@ -95,16 +109,18 @@ function love.draw()
   love.graphics.setColor(255, 255, 0)
 
   lineOffset = 0
+  local chars = 1
   for i, line in ipairs(lines) do
     charOffset = 0
 
     for j, charData in ipairs(line) do
-      if i + j == cursor then
-        love.graphics.rectangle("fill", charOffset, lineOffset, 3, 48)
+      if chars == cursor then
+        love.graphics.rectangle("fill", charOffset, lineOffset, 3, fontSize)
       end
       love.graphics.print(charData.char, charOffset, lineOffset)
 
       charOffset = charOffset + charData.width
+      chars = chars + 1
     end
     lineOffset = lineOffset + (fontSize * lineHeight)
   end
